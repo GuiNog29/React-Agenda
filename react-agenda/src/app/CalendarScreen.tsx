0b57935dacf0298ed3bf8f1743c8ed3b3035421c
Guilherme Almeida <guimathiasgui@gmail.com>
2021-09-05 22:21:34 -0300

    Modularizando a aplicação

--- react-agenda/src/app/CalendarScreen.tsx
+++ react-agenda/src/app/CalendarScreen.tsx
@@ -1,17 +1,5 @@
 import { Box } from '@material-ui/core';
-import { makeStyles } from '@material-ui/core/styles';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableContainer from '@material-ui/core/TableContainer';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
 import Button from '@material-ui/core/Button';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
-import { Icon } from '@material-ui/core';
-import { IconButton } from '@material-ui/core';
-import { Avatar } from '@material-ui/core';
 import { useEffect, useState } from 'react';
 import {
   getCalendarsEndPoint,
@@ -20,54 +8,9 @@ import {
   IEvent,
 } from './backend';
 import { useParams } from 'react-router';
-import { Link } from 'react-router-dom';
-import { formatMonth, modifyMonths } from './DateFunctions';
-
-const DAYS_OF_WEEK = ['DOM', 'SEG', 'TER', 'QUA', 'QUI', 'SEX', 'SAB'];
-
-const useStyles = makeStyles({
-  table: {
-    borderTop: "1px solid rgb(224, 224, 224)",
-    minHeight: "100%",
-    tableLayout: "fixed",
-    "& td ~ td, & th ~ th": {
-      borderLeft: "1px solid rgb(224, 224, 224)",
-    },
-    "& td": {
-      verticalAlign: "top",
-      overflow: "hidden",
-      padding: "8px 4px",
-    },
-  },
-  dayOfMonth: {
-    display: "inline-block",
-    fontWeight: 500,
-    width: "24px",
-    lineHeight: "24px",
-    marginBottom: "4px",
-    borderRadius: "50%",
-    "&.today": {
-      backgroundColor: "#3f51b5",
-      color: "white",
-    },
-  },
-  event: {
-    display: "flex",
-    alignItems: "center",
-    background: "none",
-    border: "none",
-    cursor: "pointer",
-    textAlign: "left",
-    whiteSpace: "nowrap",
-    margin: "4px 0",
-  },
-  eventBackground: {
-    display: "inline-block",
-    color: "white",
-    padding: "2px 4px",
-    borderRadius: "4px",
-  },
-});
+import CalendarsView from './CalendarsView';
+import CalendarHeader from './CalendarHeader';
+import Calendar, { ICalendarCell, IEventWithCalendar } from './Calendar';
 
 export default function CalendarScreen() {
   const [events, setEvents] = useState<IEvent[]>([]);
@@ -76,7 +19,6 @@ export default function CalendarScreen() {
 
   const { month } = useParams<{ month: string }>();
 
-  const classes = useStyles();
   const weeks = generateCalendar(
     month + '-01',
     events,
@@ -117,120 +59,20 @@ export default function CalendarScreen() {
           New Event
         </Button>
 
-        <Box marginTop="64px">
-          <h3>Schedule</h3>
-          {calendars.map((calendar, i) => (
-            <div key={calendar.id}>
-              <FormControlLabel
-                control={
-                  <Checkbox
-                    style={{ color: calendar.color }}
-                    checked={calendarsSelected[i]}
-                    onChange={() => toggleCalendar(i)}
-                  />
-                }
-                label={calendar.name}
-              />
-            </div>
-          ))}
-        </Box>
+        <CalendarsView
+          calendars={calendars}
+          toggleCalendar={toggleCalendar}
+          calendarsSelected={calendarsSelected}
+        />
       </Box>
 
       <Box display="flex" flex="1" flexDirection="column">
-        <Box display="flex" alignItems="center" padding="8px 16px">
-          <Box>
-            <IconButton
-              aria-label="Previous Month"
-              component={Link}
-              to={'/calendar/' + modifyMonths(month, -1)}
-            >
-              <Icon>chevron_left</Icon>
-            </IconButton>
+        <CalendarHeader month={month} />
 
-            <IconButton
-              aria-label="Next Month"
-              component={Link}
-              to={'/calendar/' + modifyMonths(month, +1)}
-            >
-              <Icon>chevron_right</Icon>
-            </IconButton>
-          </Box>
-
-          <Box flex="1" component="h3" marginLeft="16px">
-            {formatMonth(month)}
-          </Box>
-
-          <IconButton aria-label="Next Month">
-            <Avatar></Avatar>
-          </IconButton>
-        </Box>
-
-        <TableContainer style={{ flex: '1' }} component={'div'}>
-          <Table className={classes.table} aria-label="simple table">
-            <TableHead>
-              <TableRow>
-                {DAYS_OF_WEEK.map(day => (
-                  <TableCell align="center" key={day}>
-                    {day}
-                  </TableCell>
-                ))}
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {weeks.map((week, i) => (
-                <TableRow key={i}>
-                  {week.map(cell => (
-                    <TableCell align="center" key={cell.date}>
-                      <div className={classes.dayOfMonth}>
-                        {cell.dayOfMonth}
-                      </div>
-
-                      {cell.events.map(event => {
-                        const color = event.calendar.color;
-
-                        return (
-                          <button key={event.id} className={classes.event}>
-                            {
-                              <>
-                                <Icon style={{ color }} fontSize="inherit">
-                                  watch_later
-                                </Icon>
-                                <Box component="span" margin="0 4px">
-                                  {event.time}
-                                </Box>
-                              </>
-                            }
-                            {event.time ? (
-                              <span>{event.desc}</span>
-                            ) : (
-                              <span
-                                className={classes.eventBackground}
-                                style={{ background: color }}
-                              >
-                                {event.desc}
-                              </span>
-                            )}
-                          </button>
-                        );
-                      })}
-                    </TableCell>
-                  ))}
-                </TableRow>
-              ))}
-            </TableBody>
-          </Table>
-        </TableContainer>
+        <Calendar weeks={weeks} />
       </Box>
     </Box>
   );
-}
-
-type IEventWithCalendar = IEvent & { calendar: ICalendar };
-
-interface ICalendarCell {
-  date: string;
-  dayOfMonth: number;
-  events: IEventWithCalendar[];
 }
 
 function generateCalendar(
@@ -252,7 +94,7 @@ function generateCalendar(
   do {
     const week: ICalendarCell[] = [];
 
-    for (let i = 0; i < DAYS_OF_WEEK.length; i++) {
+    for (let i = 0; i < 7; i++) {
       const formatMonth = (currentDay.getMonth() + 1)
         .toString()
         .padStart(2, '0');
---
+++ react-agenda/src/app/CalendarsView.tsx
@@ -0,0 +1,34 @@
+import { Box } from '@material-ui/core';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import { ICalendar } from './backend';
+
+interface ICalendarsViewProps {
+  calendars: ICalendar[];
+  toggleCalendar: (i: number) => void;
+  calendarsSelected: boolean[];
+}
+
+export default function CalendarsView(props: ICalendarsViewProps) {
+  const { calendars, toggleCalendar, calendarsSelected } = props;
+
+  return (
+    <Box marginTop="64px">
+      <h3>Schedule</h3>
+      {calendars.map((calendar, i) => (
+        <div key={calendar.id}>
+          <FormControlLabel
+            control={
+              <Checkbox
+                style={{ color: calendar.color }}
+                checked={calendarsSelected[i]}
+                onChange={() => toggleCalendar(i)}
+              />
+            }
+            label={calendar.name}
+          />
+        </div>
+      ))}
+    </Box>
+  );
+}
